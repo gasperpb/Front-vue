6d846061ce1c9a5244f4de3b79acc783dc33ae17
gasperpb <you@example.com>
2021-02-11 22:56:16 -0300

    first Commit

---
+++ front-vue/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import NProgress from 'nprogress';
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Create New Employee',
+    component: ()=> import('../components/create-employee/CreateEmployeeComponent'),
+  },
+  {
+    path: '/list-employee',
+    name: 'List All Employee',
+   
+    component: () => import('../components/list-employee/ListEmployeeComponent')
+  },
+  {
+    path: '/Edit-employee',
+    name: 'Update Employee',
+   
+    component: () => import('../components/edit-employee/EditEmployeeComponent')
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(process.env.Base_url),
+  routes,
+});
+router.beforeResolve((to, from,next)=> {
+  if (to.name){
+    NProgress.start();
+  }
+  next();
+});
+router.afterEach((to,from)=>{
+  // Completa a animação da rota usada o progress bar 
+  NProgress();
+});
+
+export default router;
